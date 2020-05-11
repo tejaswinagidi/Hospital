8b4dcfe0c476d07be3d679510a5dbf56342c5b38
tejaswinagidi <62585607+tejaswinagidi@users.noreply.github.com>
2020-05-11 14:55:11 +0530

    junit test for all controller class methods

--- src/test/java/com/miniproject/Hospital/PatientControllerTest.java
+++ src/test/java/com/miniproject/Hospital/PatientControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,4 +31,45 @@ public class PatientControllerTest extends AbstractTest {
       Patient[] patienttlist = super.mapFromJson(content, Patient[].class);
       assertTrue(patienttlist.length > 0);
    }
+   @Test
+   public void addNewPatientTest() throws Exception {
+      String uri = "/AddNewPatient";
+      Patient patient = new Patient();
+      patient.setPatientId(3);
+      patient.setFirstName("teja");
+      patient.setLastName("ramani");
+      patient.setAge(22);
+      patient.setGender("f");
+      patient.setEmailid("teja.nagidi.hcl.com");
+      patient.setMobileNumber(836278910);
+      patient.setSymptoms("none");
+      String inputJson = super.mapToJson(patient);
+      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+         .contentType(MediaType.APPLICATION_JSON_VALUE)
+         .content(inputJson)).andReturn();
+      int status = mvcResult.getResponse().getStatus();
+      assertEquals(201, status);
+      String content = mvcResult.getResponse().getContentAsString();
+      assertEquals(content, "CREATED");
+   } 
+   @Test
+   public void deleteByPatientId() throws Exception {
+	      String uri = "/deleteByPatientId/2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      assertEquals(content, "OK");
+	   }
+   @Test
+   public void findByPatientId() throws Exception {
+	      String uri = "/findByPatientId/2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      assertEquals(content, "OK");
+	   }
+   
+   
 }
